d5a3dd8a4927648043237d958b6f4da8ad4574a6
francis841209 <francis841209@gmail.com>
2019-04-30 10:02:07 +0800

    Added for deploy

--- pttcrawler/prod_settings.py
+++ pttcrawler/prod_settings.py
@@ -18,7 +18,7 @@ from .settings import *
 # Heroku 的特有設定
 STATIC_ROOT = 'staticfiles'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://cryptic-basin-65238.herokuapp.com']
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
